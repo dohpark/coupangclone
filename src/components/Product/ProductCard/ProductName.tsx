bb6e499f7929238a88f874f87c17dc9b7dc5c82f
dohpark <parkdohee19931129@gmail.com>
2022-08-28 20:07:54 +0900

    feat: CardProductName 구현

--- src/components/Product/ProductCard/ProductName.tsx
+++ src/components/Product/ProductCard/ProductName.tsx
@@ -1,9 +1,20 @@
+import styled from "@emotion/styled";
+
 const ProductName: React.FC<NameProps> = ({ name }) => {
-  return <>ProductName</>;
+  return <Container>{name}</Container>;
 };
 
 interface NameProps {
   name: string;
 }
+
+const Container = styled.div`
+  font-size: 12px;
+  color: #111;
+  -webkit-line-clamp: 3;
+  text-overflow: ellipsis;
+  word-wrap: break-word;
+  text-decoration: none;
+`;
 
 export default ProductName;
