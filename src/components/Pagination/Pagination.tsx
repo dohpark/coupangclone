b6686b68d4e9df11fad6d8e504bc5a0cbf90e7bc
dohpark <parkdohee19931129@gmail.com>
2022-08-28 15:51:14 +0900

    feat: pagination 완료

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -1,5 +1,52 @@
-const Pagination = () => {
-  return <div>Pagination</div>;
+import styled from "@emotion/styled";
+
+const Pagination: React.FC<PaginationProps> = ({ page, valid, setPage }) => {
+  const onClickPrev = () => {
+    setPage(page - 1);
+  };
+  const onClickNext = () => {
+    setPage(page + 1);
+  };
+
+  return (
+    <Container>
+      <Button onClick={onClickPrev} disabled={!(page > 1)}>
+        {"<"}
+      </Button>
+      <Button>{page}</Button>
+      <Button onClick={onClickNext} disabled={!valid}>
+        {">"}
+      </Button>
+    </Container>
+  );
 };
+
+const Container = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+`;
+
+const Button = styled.button`
+  width: 31px;
+  height: 31px;
+  margin: 0 5px 0 5px;
+  color: #555;
+  border: 0.5px solid #e5e5e5;
+  background-color: #fff;
+  cursor: pointer;
+
+  &:disabled {
+    cursor: not-allowed;
+    background-color: #f8f8f8;
+    filter: brightness(90%);
+  }
+`;
+
+interface PaginationProps {
+  page: number;
+  setPage: (page: number) => void;
+  valid: boolean;
+}
 
 export default Pagination;
--- src/components/Product/Product.tsx
+++ src/components/Product/Product.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import Header from "./ProductHeader/Header";
 import Pagination from "../Pagination/Pagination";
+import axios from "axios";
 
 const Product = () => {
   const [limit, setLimit] = useState(12);
@@ -14,9 +15,17 @@ const Product = () => {
   }, [limit, page]);
 
   useEffect(() => {
-    const result = fetchData(offset, limit, sorter);
-    setData(result);
+    const getData = async () => {
+      const posts = await axios.get(
+        `https://coupang.numble.it/api/products?offset=${offset}&limit=${limit}&sorter=${sorter}`
+      );
+      setData(posts.data);
+    };
+
+    getData();
   }, [offset, limit, sorter]);
+
+  console.log(data);
 
   return (
     <>
@@ -26,13 +35,9 @@ const Product = () => {
         limit={limit}
         sorter={sorter}
       />
-      <Pagination />
+      <Pagination page={page} valid={data.length === limit} setPage={setPage} />
     </>
   );
 };
-
-function fetchData(offset: number, limit: number, sorter: string) {
-  return [];
-}
 
 export default Product;
